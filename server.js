58cde2e8377d0791d29b2da23adfdc469db4ee47
mytionBr <matheuss.jesus2017@gmail.com>
2021-01-23 04:49:28 -0300

    finalizing the project

--- server.js
+++ server.js
@@ -1,29 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const Article =  require('./models/article')
 const articleRouter = require('./routers/articles')
+const methodOverride = require('method-override')
 const app = express();
 
-mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true})
 
 app.set('view engine', 'ejs')
-
 app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
 
-
-
-app.get('/', (req, res) => {
-    const articles = [
-        {
-            title: "Java 8",
-            createdAt: new Date(),
-            description: 'Text description'
-        },
-        {
-            title: "PHP 7",
-            createdAt: new Date(),
-            description: 'Text description'
-        }
-    ]
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort(
+          {
+            createdAt:'desc'
+           }
+        )
     res.render('articles/index', { articles: articles })
 })
 
